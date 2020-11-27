15348abe91df12bfcae3b736565fa88cc5b44e1e
marina-sp <speranskaya@cis.lmu.de>
2020-11-27 17:47:06 +0100

    add weak supervision training script

---
+++ scripts/train/train_kn_weak.sh
@@ -0,0 +1,18 @@
+# Train DyGIE++ model on the scierc data set.
+# Usage: bash scripts/train/train_scierc.sh [gpu-id]
+# gpu-id can be an integer GPU ID, or -1 for CPU.
+
+experiment_name="kn_tac"
+data_root="./data/weak_sv"
+config_file="./training_config/kn_working_example.jsonnet"
+cuda_device=$1
+
+# Train model.
+ie_train_data_path=$data_root/tac.train_patterns.json \
+    ie_dev_data_path=$data_root/kn.train.dev.json \
+    ie_test_data_path=$data_root/kn.dev.json \
+    cuda_device=$cuda_device \
+    allennlp train $config_file \
+    --cache-directory $data_root/cached \
+    --serialization-dir ./models/$experiment_name \
+    --include-package dygie
